852a87c59c3f12ac821687076f23ee3227f0a4f7
josh-hensley <me@joshhensley.com>
2024-11-26 15:55:33 -0600

    starting point

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,12 +1,18 @@
 // src/App.tsx
-import React from "react";
+import React, { useState } from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
 import BrowseRecipes from "./pages/BrowseRecipes.tsx";
 import Dashboard from "./pages/Dashboard.tsx";
-import Login from "./pages/Login";
 import SignUp from "./pages/Signup";
 import Navbar from "./components/Navbar";
+<<<<<<< Updated upstream
+=======
+import CreateMealPlan from "./pages/CreateMealPlan";
+import ViewMealPlan from "./pages/ViewMealPlan";
+import { Button } from 'react-bootstrap';
+import LoginModal from './pages/Login';
+>>>>>>> Stashed changes
 
 const App: React.FC = () => {
   return (
--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 // src/components/Navbar.tsx
 import React from "react";
 import { Link } from "react-router-dom";
@@ -13,5 +14,66 @@ const Navbar: React.FC = () => (
     </ul>
   </nav>
 );
+=======
+import 'bootstrap/dist/css/bootstrap.min.css';  // for styling
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';  // for JavaScript functionality (includes popper)
+import Login from "../pages/Login"
+import { Button } from 'bootstrap';
+import { Link } from 'react-router-dom';
+
+const Navbar: React.FC = () => {
+
+  return (
+    <>
+    <Login />
+    <nav className="navbar navbar-expand-lg navbar-light bg-light">
+      <div className="container-fluid">
+        <Link className="navbar-brand" to="/">
+          The Cook's Compass
+        </Link>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarNav"
+          aria-controls="navbarNav"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarNav">
+          <ul className="navbar-nav ms-auto">
+            <li className="nav-item">
+              <Link className="nav-link" to="/" >
+                Home
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link" to="/browse">
+                Browse Recipes
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link" to="/dashboard">
+                Dashboard
+              </Link>
+            </li>
+            <li className="nav-item">
+              
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link" to="/signup">
+                Sign-Up
+              </Link>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+    </>
+  );
+};
+>>>>>>> Stashed changes
 
 export default Navbar;
