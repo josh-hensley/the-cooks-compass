c2b4fc254e6bd6cdaff0e65f7d450c2f69c91a24
josh-hensley <me@joshhensley.com>
2024-11-27 15:23:43 -0600

    created interfaces for login and user data, util for authorization

---
+++ client/src/interfaces/UserData.tsx
@@ -0,0 +1,8 @@
+export interface UserData {
+    id: number | null;
+    username: string | null;
+    email: string | null;
+    meal_plan_ids: number[] | null;
+    favorite_ids: number[] | null;
+  }
+  
