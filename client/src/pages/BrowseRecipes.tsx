83efdefc130d7b44e17dce4d81630ccfffc6f93e
josh-hensley <me@joshhensley.com>
2024-11-27 18:51:56 -0600

    added login check to browse page

--- client/src/pages/BrowseRecipes.tsx
+++ client/src/pages/BrowseRecipes.tsx
@@ -1,6 +1,24 @@
+import React, { useState, useEffect } from 'react';
+import auth from '../utils/auth.js';
 
-import React from 'react';
-
-const BrowseRecipes: React.FC = () => <h1>Browse Recipes Page</h1>;
+const BrowseRecipes: React.FC = () => {
+    const [loginCheck, setLoginCheck] =  useState(false);
+    const checkLogin = () => {
+        if (auth.loggedIn()){
+            setLoginCheck(true);
+        }
+    }
+    useEffect(()=>{
+        checkLogin();
+    }, [loginCheck])
+    return (
+        <>{
+            !loginCheck ? (
+                <h1>Login to view Recipes</h1>
+            ):(
+                <h1>Browse Recipes Page</h1>
+            )
+        }</>
+)};
 
 export default BrowseRecipes;
